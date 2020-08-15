a88f7723800d0697320f72a7222e42251605cc0f
dzhaanbaeva <dzhaanbaeva@gmail.com>
2020-08-15 10:28:02 +0600

    create securityConfig, error controller

---
+++ src/main/java/com/exam10/Exam10Application.java
@@ -0,0 +1,13 @@
+package com.exam10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exam10Application.class, args);
+    }
+
+}
