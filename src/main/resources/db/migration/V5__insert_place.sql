66c0f98d133385a37c752105adc7ddbeb34cb72b
dzhaanbaeva <dzhaanbaeva@gmail.com>
2020-08-15 13:02:25 +0600

    download images and change default data

--- src/main/resources/db/migration/V5__insert_place.sql
+++ src/main/resources/db/migration/V5__insert_place.sql
@@ -1,17 +1,17 @@
-insert into places(id, name, description)
-VALUES (1, 'CafeDeParis', 'Good cafe in Bishkek');
+insert into places(id, name, photo, description)
+VALUES (1, 'CafeDeParis', '/images/cafeDeParis.png','Good cafe in Bishkek');
 
-insert into places(id, name, description)
-VALUES (2, 'Navigator', 'Nice place');
+insert into places(id, name, photo, description)
+VALUES (2, 'Navigator','/images/navigator.jpg', 'Nice place');
 
-insert into places(id, name, description)
-VALUES (3, 'Kaynar', 'Perfect');
+insert into places(id, name, photo, description)
+VALUES (3, 'Kaynar','/images/kaynar.jpg', 'Perfect');
 
-insert into places(id, name, description)
-VALUES (4, 'Kent', 'Place for friends');
+insert into places(id, name, photo, description)
+VALUES (4, 'Kent', '/images/kent.jpg','Place for friends');
 
-insert into places(id, name, description)
-VALUES (5, 'Social', 'Very good cafe');
+insert into places(id, name, photo, description)
+VALUES (5, 'Social', '/images/social.jpg','Very good cafe');
 
 
 
